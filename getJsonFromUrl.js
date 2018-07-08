d9cdb8b3ce98f4098a099eca30a6d402db7f8b47
Jack Scott <jtreyetsy@gmail.com>
2018-07-07 19:03:49 -0500

    space
    space

---
+++ getJsonFromUrl.js
@@ -0,0 +1,21 @@
+function getJsonFromUrl() {
+  var query = location.search.substr(1);
+  var result = {};
+  query.split("&").forEach(function(part) {
+    var item = part.split("=");
+    result[item[0]] = decodeURIComponent(item[1]);
+  });
+  return result;
+}
+
+//Creates Javascript Json 
+g = getJsonFromUrl()
+
+
+outputParameters
+// takes g and outputs all pramas
+for (var key in g) {
+    if (p.hasOwnProperty(key)) {
+        console.log(key + " -> " + p[key]);
+    }
+}
